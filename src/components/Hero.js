ec849b07772c4baf4f36f4ec1c47b3587fe2a91e
programwithtanya <140419966+programwithtanya@users.noreply.github.com>
2024-07-22 21:06:38 +0530

    updated

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -22,7 +22,11 @@ export default function Hero() {
     </Link>
      </span>
      <span>
+     <Link to={"/ProductSlider"} className='duration-500 '>
+
      <i className="fa-solid fa-arrow-right text-black"></i>
+     </Link>
+
      </span>
       </button>
     </div>
